694110d02cdfbe1465578ea834a54ce22b62453d
NishigandhaNEU <114119400+NishigandhaNEU@users.noreply.github.com>
2023-12-01 16:00:45 -0800

    Added Transaction in Sps

--- P4/Stored Procedures/Stored Procedure 1.sql
+++ P4/Stored Procedures/Stored Procedure 1.sql
@@ -1,3 +1,15 @@
+-- Create ErrorLog table if not exists
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'ErrorLog')
+BEGIN
+    CREATE TABLE ErrorLog (
+        LogId INT PRIMARY KEY IDENTITY(1,1),
+        ErrorMessage NVARCHAR(MAX),
+        ErrorSeverity INT,
+        ErrorState INT
+    );
+END
+GO
+
 -- Procedure 1: Create stored procedure to get Flora information based on Habitat ID
 CREATE PROCEDURE GetFloraInfoByHabitatID
     @InputHabitatID INT,
@@ -5,33 +17,57 @@ CREATE PROCEDURE GetFloraInfoByHabitatID
     @OutputFloraSpecies VARCHAR(255) OUT
 AS
 BEGIN
-    SET NOCOUNT ON;
+    BEGIN TRY
+        BEGIN TRANSACTION;
 
-    -- Declare variables to store the output values
-    DECLARE @FloraID INT;
-    DECLARE @FloraSpecies VARCHAR(255);
+        -- Declare variables to store the output values
+        DECLARE @FloraID INT;
+        DECLARE @FloraSpecies VARCHAR(255);
 
-    -- Retrieve Flora information based on Habitat ID
-    SELECT
-        @FloraID = F.Flora_Id,
-        @FloraSpecies = F.Species
-    FROM
-        HABITAT H
-    INNER JOIN
-        FLORA_HABITAT FH ON H.Habitat_Id = FH.Habitat_Id
-    INNER JOIN
-        FLORA F ON FH.Flora_Id = F.Flora_Id
-    WHERE
-        H.Habitat_Id = @InputHabitatID;
+        -- Retrieve Flora information based on Habitat ID
+        SELECT
+            @FloraID = F.Flora_Id,
+            @FloraSpecies = F.Species
+        FROM
+            HABITAT H
+        INNER JOIN
+            FLORA_HABITAT FH ON H.Habitat_Id = FH.Habitat_Id
+        INNER JOIN
+            FLORA F ON FH.Flora_Id = F.Flora_Id
+        WHERE
+            H.Habitat_Id = @InputHabitatID;
 
-    -- Assign values to output parameters
-    SET @OutputFloraID = @FloraID;
-    SET @OutputFloraSpecies = @FloraSpecies;
+        -- Check if Flora information is found
+        IF (@FloraID IS NULL)
+        BEGIN
+            -- Raise an error and terminate the stored procedure
+            THROW 50000, 'Flora information not found for the specified Habitat ID.', 1;
+        END
+
+        -- Assign values to output parameters
+        SET @OutputFloraID = @FloraID;
+        SET @OutputFloraSpecies = @FloraSpecies;
+
+        COMMIT; -- Commit the transaction
+    END TRY
+    BEGIN CATCH
+        -- Rollback the transaction
+        IF @@TRANCOUNT > 0
+            ROLLBACK;
+
+        -- Log the error
+        INSERT INTO ErrorLog (ErrorMessage, ErrorSeverity, ErrorState)
+        VALUES (ERROR_MESSAGE(), ERROR_SEVERITY(), ERROR_STATE());
+
+        -- Re-throw the error to the calling application
+        THROW;
+    END CATCH;
 END;
 
 
+
 --************************COMMAND TO EXECUTE Procedure 1************************
---DECLARE @HabitatID INT = 2;
+--DECLARE @HabitatID INT = 1;
 --DECLARE @FloraIDResult INT;
 --DECLARE @FloraSpeciesResult VARCHAR(255);
 
--- P4/Stored Procedures/Stored Procedure 3.sql
+++ P4/Stored Procedures/Stored Procedure 3.sql
@@ -10,49 +10,74 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    -- Check if the Guide ID and Sanctuary ID exist
-    IF NOT EXISTS (SELECT 1 FROM GUIDE WHERE Guide_ID = @GuideID)
-        OR NOT EXISTS (SELECT 1 FROM SANCTUARY WHERE Sanctuary_Id = @SanctuaryID)
-    BEGIN
-        SET @OutputMessage = 'Error: Invalid Guide ID or Sanctuary ID.';
-        RETURN;
-    END;
+    -- Begin Transaction
+    BEGIN TRANSACTION;
 
-    -- Check if the phone number is valid (numeric and 10 digits)
-    IF LEN(@PhoneNumber) <> 10 OR ISNUMERIC(@PhoneNumber) = 0
-    BEGIN
-        SET @OutputMessage = 'Error: Invalid phone number.';
-        RETURN;
-    END;
+    BEGIN TRY
+        -- Check if the Guide ID and Sanctuary ID exist
+        IF NOT EXISTS (SELECT 1 FROM GUIDE WHERE Guide_ID = @GuideID)
+            OR NOT EXISTS (SELECT 1 FROM SANCTUARY WHERE Sanctuary_Id = @SanctuaryID)
+        BEGIN
+            SET @OutputMessage = 'Error: Invalid Guide ID or Sanctuary ID.';
+            -- Rollback the transaction
+            ROLLBACK;
+            RETURN;
+        END;
 
-    -- Check if the email is valid
-    IF NOT @Email LIKE '%_@__%.%'
-    BEGIN
-        SET @OutputMessage = 'Error: Invalid email address.';
-        RETURN;
-    END;
+        -- Check if the phone number is valid (numeric and 10 digits)
+        IF LEN(@PhoneNumber) <> 10 OR ISNUMERIC(@PhoneNumber) = 0
+        BEGIN
+            SET @OutputMessage = 'Error: Invalid phone number.';
+            -- Rollback the transaction
+            ROLLBACK;
+            RETURN;
+        END;
 
-    -- Insert the new tourist into the TOURIST table
-    INSERT INTO TOURIST (Tourist_Id, Guide_Id, Sanctuary_Id, Tourist_Name, Phone_Number, Email)
-    VALUES (
-        (SELECT ISNULL(MAX(Tourist_Id), 0) + 1 FROM TOURIST),
-        @GuideID,
-        @SanctuaryID,
-        @TouristName,
-        @PhoneNumber,
-        @Email
-    );
+        -- Check if the email is valid
+        IF NOT @Email LIKE '%_@__%.%'
+        BEGIN
+            SET @OutputMessage = 'Error: Invalid email address.';
+            -- Rollback the transaction
+            ROLLBACK;
+            RETURN;
+        END;
 
-    SET @OutputMessage = 'Tourist created successfully.';
+        -- Insert the new tourist into the TOURIST table
+        INSERT INTO TOURIST (Tourist_Id, Guide_Id, Sanctuary_Id, Tourist_Name, Phone_Number, Email)
+        VALUES (
+            (SELECT ISNULL(MAX(Tourist_Id), 0) + 1 FROM TOURIST),
+            @GuideID,
+            @SanctuaryID,
+            @TouristName,
+            @PhoneNumber,
+            @Email
+        );
+
+        SET @OutputMessage = 'Tourist created successfully.';
+        -- Commit the transaction
+        COMMIT;
+    END TRY
+    BEGIN CATCH
+        -- Rollback the transaction
+        ROLLBACK;
+
+        -- Log the error
+        INSERT INTO ErrorLog (ErrorMessage, ErrorSeverity, ErrorState)
+        VALUES (ERROR_MESSAGE(), ERROR_SEVERITY(), ERROR_STATE());
+
+        -- Re-throw the error to the calling application
+        THROW;
+    END CATCH;
 END;
 
 --************************COMMAND TO EXECUTE Procedure 3************************
 --DECLARE @TouristName VARCHAR(255) = 'John Doe New';
 --DECLARE @PhoneNumber VARCHAR(20) = '1234567890';
 --DECLARE @Email VARCHAR(255) = 'john.doe@hotmail.com';
---DECLARE @GuideID INT = 5; -- Replace with the actual Guide ID
+--DECLARE @GuideID INT = 2; -- Replace with the actual Guide ID
 --DECLARE @SanctuaryID INT = 1; -- Replace with the actual Sanctuary ID
 --DECLARE @OutputMessage VARCHAR(100);
+
 --EXEC AddNewTourist
 --    @TouristName = @TouristName,
 --    @PhoneNumber = @PhoneNumber,
@@ -60,8 +85,9 @@ END;
 --    @GuideID = @GuideID,
 --    @SanctuaryID = @SanctuaryID,
 --    @OutputMessage = @OutputMessage OUTPUT;
-----Display the output message
+
+---- Display the output message
 --PRINT @OutputMessage;
 
---select * from TOURIST where Tourist_Name='John Doe New'
-
+---- Check the inserted records
+--SELECT * FROM TOURIST WHERE Tourist_Name = 'John Doe New';
